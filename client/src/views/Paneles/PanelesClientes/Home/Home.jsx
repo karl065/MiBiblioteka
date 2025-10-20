ebc40b67c6d7e2841e2b9c221f0983139b4faf01
Carlos Castellanos <karlos065@hotmail.com>
2025-10-20 18:32:32 -0500

    Estilizar Home

--- client/src/views/Paneles/PanelesClientes/Home/Home.jsx
+++ client/src/views/Paneles/PanelesClientes/Home/Home.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-
 const Home = () => {
 	return (
-		<div>
-			<nav className="p-2 bg-black rounded-lg opacity-70">
-				<div className="bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
-					<div className="flex justify-center p-2 border-2 border-black rounded-lg">
-						<div className="p-2">
-							<div className="font-bold text-white">
+		<div className="h-full w-full ">
+			<nav className="h-full p-2 bg-black rounded-lg opacity-70">
+				<div className="h-full bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+					<div className="h-full flex justify-center p-2 border-2 border-black rounded-lg">
+						<div className="p-2 border-2 border-pink-700 w-full h-full">
+							<div className="flex items-center justify-center border-2 border-pink-700 font-bold text-white">
 								<span>Home</span>
 							</div>
 							<hr className="my-2 border-2" />
